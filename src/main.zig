90c9be51470ce77802dd639a0bd51fae0f8274eb
c0utin <rafaelcouto111@gmail.com>
2024-11-06 17:44:27 -0300

    feat: learning raylib & update build

--- src/main.zig
+++ src/main.zig
@@ -6,11 +6,30 @@ const r = @cImport({
 pub fn main() !void {
     r.InitWindow(960, 540, "poggers");
     r.SetTargetFPS(144);
+
+    var rotation: f32 = 0.0;
+
     defer r.CloseWindow();
 
     while (!r.WindowShouldClose()) {
+        rotation += 0.8;
+
+        // DRAW START
         r.BeginDrawing();
+
+        r.ClearBackground(r.WHITE);
+        r.DrawText("bela bola", 20, 20, 20, r.GRAY);
+
+        r.DrawCircle(960 / 5, 120, 35, r.RED);
+        r.DrawCircleGradient(960 / 5, 220, 60, r.GREEN, r.SKYBLUE);
+        r.DrawCircleLines(960 / 5, 340, 80, r.DARKBLUE);
+
+        r.DrawPoly((r.Vector2){ .x = 960 / 4.0 * 3, .y = 330 }, 6, 80, rotation, r.BROWN);
+        r.DrawPolyLines((r.Vector2){ .x = 960 / 4.0 * 3, .y = 330 }, 6, 90, rotation, r.BROWN);
+        r.DrawPolyLinesEx((r.Vector2){ .x = 960 / 4.0 * 3, .y = 330 }, 6, 85, rotation, 6, r.BEIGE);
+
         r.ClearBackground(r.BLACK);
         r.EndDrawing();
+        // DRAW END
     }
 }
